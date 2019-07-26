55fbc94fb8c188bb1e510976a6f784a8fe64a208
KamaljeetSahoo <kamaljeetsahoo@gmail.com>
2019-07-26 19:46:33 +0530

    succesful implementation of front end in codeigniter and database connected succesfully

---
+++ application/views/templates/footer.php
@@ -0,0 +1,110 @@
+<!-- FOOTER -->
+    <div class="navbar navbar-inverse navbar-static-bottom">
+      <div class="container">
+        
+        <div class="navbar-text pull-left">
+          <p>Copyright &copy; 2019 - MondayMorningNITRKL</p>
+        </div>
+
+        <div class="navbar-text pull-right">
+          <a href="#"><i class="fa fa-facebook fa-2x"></i></a>
+          <a href="#"><i class="fa fa-google fa-2x" aria-hidden="true"></i></a>
+          <a href="#"><i class="fa fa-instagram fa-2x" aria-hidden="true"></i></a>
+          <a href="#"><i class="fa fa-twitter fa-2x" aria-hidden="true"></i></a>
+        </div>
+
+      </div>
+    </div>
+
+<script>
+  var input = document.getElementById("myInput");
+  var text = document.getElementById("text");
+  input.addEventListener("keyup", function(event) {
+
+  if (event.getModifierState("CapsLock")) {
+      text.style.display = "block";
+    } else {
+      text.style.display = "none"
+    }
+  });
+
+  function myFunction() {
+  var x = document.getElementById("myinput");
+  if (x.type === "password") {
+    x.type = "text";
+  }
+  else {
+    x.type = "password";
+  }
+  }
+</script>
+<script>
+  function show_more_function(){
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("Read_more_btn");
+
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
+  function show_more_function_1(){
+    var dots_1 = document.getElementById("dots_1");
+    var moreText_1 = document.getElementById("more_1");
+    var btnText_1 = document.getElementById("Read_more_btn_1");
+
+    if (dots_1.style.display === "none") {
+      dots_1.style.display = "inline";
+      btnText_1.innerHTML = "Read more"; 
+      moreText_1.style.display = "none";
+    } else {
+      dots_1.style.display = "none";
+      btnText_1.innerHTML = "Read less"; 
+      moreText_1.style.display = "inline";
+    }
+  }
+  function show_more_function_2(){
+    var dots_2 = document.getElementById("dots_2");
+    var moreText_2 = document.getElementById("more_2");
+    var btnText_2 = document.getElementById("Read_more_btn_2");
+
+    if (dots_2.style.display === "none") {
+      dots_2.style.display = "inline";
+      btnText_2.innerHTML = "Read more"; 
+      moreText_2.style.display = "none";
+    } else {
+      dots_2.style.display = "none";
+      btnText_2.innerHTML = "Read less"; 
+      moreText_2.style.display = "inline";
+    }
+  }
+
+  function show_more_function_3(){
+    var dots_3 = document.getElementById("dots_3");
+    var moreText_3 = document.getElementById("more_3");
+    var btnText_3 = document.getElementById("Read_more_btn_3");
+
+    if (dots_3.style.display === "none") {
+      dots_3.style.display = "inline";
+      btnText_3.innerHTML = "Read more"; 
+      moreText_3.style.display = "none";
+    } else {
+      dots_3.style.display = "none";
+      btnText_3.innerHTML = "Read less"; 
+      moreText_3.style.display = "inline";
+    }
+  }
+</script>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="<?php echo base_url(); ?>assets/js/bootstrap.js"></script>
+  </body>
+</html>
---
+++ application/views/templates/header.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>NEWS WEBSITE</title>
+
+    <!-- Bootstrap -->
+    <link href="<?php echo base_url(); ?>assets/css/bootstrap.css" rel="stylesheet">
+
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/style.css">
+
+     <!-- FONT AWESOME -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <style>
+    #text {display:none;color:red}
+    </style>
+  </head>
+  <body>
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <span class="sr-only">Toggle Navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">Website</a>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-left">
+            <li><a href="<?php echo base_url(); ?>home">HOME</a></li>
+            <li><a href="<?php echo base_url(); ?>about">ARTICLES</a></li>
+            <li><a href="#">BULLETIN</a></li>
+            <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">CATEGORIES
+              <b class="caret"></b>
+              <ul class="dropdown-menu">
+                <li class="dropdown-header">Sub-Categories</li>
+                <li><a href="#">SOCIAL</a></li>
+                <li><a href="#">POLITICAL</a></li>
+                <li><a href="#">TECHNICAL</a></li>
+                <li class="dropdown-header">Sub-Categories</li>
+                <li><a href="#">SOCIAL</a></li>
+                <li><a href="#">POLITICAL</a></li>
+                <li><a href="#">TECHNICAL</a></li>
+              </ul>
+            </li>
+          </ul>
+          <div class="col-sm-3 col-md-3 pull-right">
+            <form class="navbar-form" role="search">
+            <div class="input-group">
+                <input type="text" class="form-control" placeholder="Search" name="srch-term" id="srch-term">
+                <div class="input-group-btn">
+                    <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+                </div>
+            </div>
+            </form>
+          </div>
+        </div>  
+      </div>  
+    </nav>
